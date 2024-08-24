3f274538f79a39fa1f5cd84cb72f3394d0abd2f6
shakeel.ahmed <shakeel.ahmed@bajcotechnologies.com>
2024-08-24 16:43:35 +0500

    remove extrz spacing

--- ViewControllers/Delivery/RatingViewController/RatingViewController.swift
+++ ViewControllers/Delivery/RatingViewController/RatingViewController.swift
@@ -201,9 +201,6 @@ extension RatingViewController: UITableViewDataSource, UITableViewDelegate {
     }
 }
 
-
-
-
 extension RatingViewController {
     // MARK: - ModelGetByType
     // MARK: - ModelGetByType
